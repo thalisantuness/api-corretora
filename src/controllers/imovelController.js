88f279aaf900980c0b8e39a93a4f0d6fdc625b63
Thalis Antunes <thalisantunessilva@gmail.com>
2025-03-21 19:44:45 -0300

    fix: resolucao função de apagar imovel associada a imagens

--- src/controllers/imovelController.js
+++ src/controllers/imovelController.js
@@ -1,60 +1,57 @@
-const imovelRepository = require('../repositories/imovelRepository');
-const sharp = require('sharp');
-const s3 = require('../utils/awsConfig'); 
-const { v4: uuidv4 } = require('uuid');
+const imovelRepository = require("../repositories/imovelRepository");
+const sharp = require("sharp");
+const s3 = require("../utils/awsConfig");
+const { v4: uuidv4 } = require("uuid");
 
 function ImovelController() {
-  
   async function getImovel(req, res) {
     try {
-      const { tipo_id, cidade_id, n_quartos, n_banheiros, n_vagas, estado_id } = req.query;
-  
-    
+      const { tipo_id, cidade_id, n_quartos, n_banheiros, n_vagas, estado_id } =
+        req.query;
+
       const filtros = {};
-  
+
       if (tipo_id) filtros.tipo_id = tipo_id;
       if (cidade_id) filtros.cidade_id = cidade_id;
       if (n_quartos) filtros.n_quartos = n_quartos;
       if (n_banheiros) filtros.n_banheiros = n_banheiros;
       if (n_vagas) filtros.n_vagas = n_vagas;
       if (estado_id) filtros.estado_id = estado_id;
-  
+
       const imoveis = await imovelRepository.listarImovel(filtros);
-  
+
       if (imoveis.length === 0) {
-        return res.status(404).json({ error: 'Nenhum imóvel encontrado com os filtros fornecidos' });
+        return res
+          .status(404)
+          .json({
+            error: "Nenhum imóvel encontrado com os filtros fornecidos",
+          });
       }
-  
+
       res.json(imoveis);
     } catch (error) {
-      console.error('Erro ao obter imóveis:', error);
-      res.status(500).json({ error: 'Erro ao obter imóveis' });
+      console.error("Erro ao obter imóveis:", error);
+      res.status(500).json({ error: "Erro ao obter imóveis" });
     }
   }
-  
-
 
   async function compressImage(buffer) {
-    return sharp(buffer)
-      .resize(500, 500) 
-      .jpeg({ quality: 80 }) 
-      .toBuffer();
+    return sharp(buffer).resize(500, 500).jpeg({ quality: 80 }).toBuffer();
   }
 
   async function uploadToS3(buffer, fileName) {
     const params = {
-      Bucket: process.env.AWS_BUCKET_NAME, 
+      Bucket: process.env.AWS_BUCKET_NAME,
       Key: `imoveis/${fileName}`,
       Body: buffer,
-      ContentType: 'image/jpeg', 
-      ACL: 'public-read', 
+      ContentType: "image/jpeg",
+      ACL: "public-read",
     };
-  
+
     const result = await s3.upload(params).promise();
 
     return result.Location;
   }
-
 
   async function postImovel(req, res) {
     try {
@@ -71,18 +68,17 @@ function ImovelController() {
         estado_id,
         imagemBase64,
       } = req.body;
-  
-  
-      const imagemBuffer = Buffer.from(imagemBase64.split(',')[1], 'base64');
-  
+
+      const imagemBuffer = Buffer.from(imagemBase64.split(",")[1], "base64");
+
       const imagemComprimida = await compressImage(imagemBuffer);
-  
+
       const uniqueFileName = `${uuidv4()}.jpg`;
 
       const imageUrl = await uploadToS3(imagemComprimida, uniqueFileName);
 
       console.log("AQUIIII " + JSON.stringify(imageUrl));
-  
+
       const novoImovel = await imovelRepository.criarImovel({
         nome,
         description,
@@ -96,14 +92,16 @@ function ImovelController() {
         estado_id,
         imageData: imageUrl,
       });
-  
-      res.json({ message: `Imóvel ${nome} cadastrado com sucesso!`, imovel: novoImovel });
+
+      res.json({
+        message: `Imóvel ${nome} cadastrado com sucesso!`,
+        imovel: novoImovel,
+      });
     } catch (error) {
-      console.error('Erro ao cadastrar imóvel:', error);
-      res.status(500).json({ error: 'Erro ao cadastrar imóvel' });
+      console.error("Erro ao cadastrar imóvel:", error);
+      res.status(500).json({ error: "Erro ao cadastrar imóvel" });
     }
   }
-  
 
   async function getImovelById(req, res) {
     try {
@@ -111,11 +109,11 @@ function ImovelController() {
       const imovel = await imovelRepository.buscarImovelPorId(id);
 
       if (!imovel) {
-        return res.status(404).json({ error: 'Imóvel não encontrado' });
+        return res.status(404).json({ error: "Imóvel não encontrado" });
       }
 
       const imageBase64 = imovel.imageData
-        ? Buffer.from(imovel.imageData).toString('base64')
+        ? Buffer.from(imovel.imageData).toString("base64")
         : null;
 
       res.json({
@@ -123,29 +121,57 @@ function ImovelController() {
         image: imageBase64 ? `data:image/png;base64,${imageBase64}` : null,
       });
     } catch (error) {
-      console.error('Erro ao buscar imóvel:', error);
-      res.status(500).json({ error: 'Erro ao buscar imóvel' });
+      console.error("Erro ao buscar imóvel:", error);
+      res.status(500).json({ error: "Erro ao buscar imóvel" });
     }
   }
 
   async function putImovel(req, res) {
     try {
       const { id } = req.params;
-      const { nome, description, valor, valor_condominio, n_quartos, n_banheiros, n_vagas, tipo_id, cidade_id, estado_id } = req.body;
+      const {
+        nome,
+        description,
+        valor,
+        valor_condominio,
+        n_quartos,
+        n_banheiros,
+        n_vagas,
+        tipo_id,
+        cidade_id,
+        estado_id,
+      } = req.body;
       const imagemBase64 = req.file ? req.file.buffer : null;
 
-      const dadosAtualizados = { nome, description, valor, valor_condominio, n_quartos, n_banheiros, n_vagas, tipo_id, cidade_id, estado_id };
+      const dadosAtualizados = {
+        nome,
+        description,
+        valor,
+        valor_condominio,
+        n_quartos,
+        n_banheiros,
+        n_vagas,
+        tipo_id,
+        cidade_id,
+        estado_id,
+      };
 
       if (imagemBase64) {
         dadosAtualizados.imageData = await compressImage(imagemBase64);
       }
 
-      const imovelAtualizado = await imovelRepository.atualizarImovel(id, dadosAtualizados);
+      const imovelAtualizado = await imovelRepository.atualizarImovel(
+        id,
+        dadosAtualizados
+      );
 
-      res.json({ message: 'Imóvel atualizado com sucesso!', imovel: imovelAtualizado });
+      res.json({
+        message: "Imóvel atualizado com sucesso!",
+        imovel: imovelAtualizado,
+      });
     } catch (error) {
-      console.error('Erro ao atualizar imóvel:', error);
-      res.status(500).json({ error: 'Erro ao atualizar imóvel' });
+      console.error("Erro ao atualizar imóvel:", error);
+      res.status(500).json({ error: "Erro ao atualizar imóvel" });
     }
   }
 
@@ -153,23 +179,32 @@ function ImovelController() {
     try {
       const { id } = req.params;
 
+      const imagens = await imovelRepository.buscarImagensPorImovelId(id);
+
+      for (const imagem of imagens) {
+        await imovelRepository.deletarImagem(imagem.photo_id);
+
+        const key = imagem.imageData.split(".com/")[1];
+        const params = { Bucket: process.env.AWS_BUCKET_NAME, Key: key };
+
+        await s3.deleteObject(params).promise();
+      }
+
       await imovelRepository.deletarImovel(id);
 
-      res.json({ message: 'Imóvel excluído com sucesso!' });
+      res.json({ message: "Imóvel e imagens excluídos com sucesso!" });
     } catch (error) {
-      console.error('Erro ao excluir imóvel:', error);
-      res.status(500).json({ error: 'Erro ao excluir imóvel' });
+      console.error("Erro ao excluir imóvel:", error);
+      res.status(500).json({ error: "Erro ao excluir imóvel" });
     }
   }
 
- 
-  
   return {
     getImovel,
     postImovel,
     getImovelById,
     putImovel,
-    deleteImovel
+    deleteImovel,
   };
 }
 
--- src/repositories/imovelRepository.js
+++ src/repositories/imovelRepository.js
@@ -1,17 +1,17 @@
-const { Imovel } = require('../model/Imovel');
-const {Estado} = require('../model/Estado');
-const {Tipo} = require('../model/Tipo');
-const {Photo} = require('../model/Photo');
-const {Cidade} = require('../model/Cidade');
-//
+const { Imovel } = require("../model/Imovel");
+const { Estado } = require("../model/Estado");
+const { Tipo } = require("../model/Tipo");
+const { Photo } = require("../model/Photo");
+const { Cidade } = require("../model/Cidade");
+
 async function listarImovel(filtros = {}) {
   return await Imovel.findAll({
     where: filtros,
     include: [
-      { model: Estado, as: 'estado', attributes: ['estado_id', 'nome'] },
-      { model: Cidade, as: 'cidade', attributes: ['cidade_id', 'nome'] },
-      { model: Tipo, as: 'tipo', attributes: ['tipo_id', 'nome'] },
-      { model: Photo, as: 'photo', attributes: ['photo_id', 'imageData'] }
+      { model: Estado, as: "estado", attributes: ["estado_id", "nome"] },
+      { model: Cidade, as: "cidade", attributes: ["cidade_id", "nome"] },
+      { model: Tipo, as: "tipo", attributes: ["tipo_id", "nome"] },
+      { model: Photo, as: "photos", attributes: ["photo_id", "imageData"] },
     ],
   });
 }
@@ -19,73 +19,68 @@ async function listarImovel(filtros = {}) {
 async function buscarImovelPorId(id) {
   const imovel = await Imovel.findByPk(id, {
     include: [
-      { model: Estado, as: 'estado', attributes: ['estado_id', 'nome'] },
-      { model: Cidade, as: 'cidade', attributes: ['cidade_id', 'nome'] },
-      { model: Tipo, as: 'tipo', attributes: ['tipo_id', 'nome'] },
-      { model: Photo, as: 'photo', attributes: ['photo_id', 'imageData'] }
+      { model: Estado, as: "estado", attributes: ["estado_id", "nome"] },
+      { model: Cidade, as: "cidade", attributes: ["cidade_id", "nome"] },
+      { model: Tipo, as: "tipo", attributes: ["tipo_id", "nome"] },
+      { model: Photo, as: "photos", attributes: ["photo_id", "imageData"] },
     ],
   });
 
   if (!imovel) {
-    throw new Error('Imóvel não encontrado');
+    throw new Error("Imóvel não encontrado");
   }
 
   return imovel;
 }
 
-
 async function criarImovel(dadosImovel) {
-  const {
-    nome,
-    description,
-    valor,
-    valor_condominio,
-    n_quartos,
-    n_banheiros,
-    n_vagas,
-    tipo_id,
-    cidade_id,
-    estado_id,
-    imageData,
-  } = dadosImovel;
-
-  const imovel = await Imovel.create({
-    nome,
-    description,
-    valor,
-    valor_condominio,
-    n_quartos,
-    n_banheiros,
-    n_vagas,
-    tipo_id,
-    cidade_id,
-    estado_id,
-    imageData, 
-  });
-
-  return imovel;
+  return await Imovel.create(dadosImovel);
 }
 
 async function atualizarImovel(id, dadosAtualizados) {
   const imovel = await Imovel.findByPk(id);
 
   if (!imovel) {
-    throw new Error('Imóvel não encontrado');
+    throw new Error("Imóvel não encontrado");
   }
 
   await imovel.update(dadosAtualizados);
   return imovel;
 }
 
+async function buscarImagensPorImovelId(imovelId) {
+  return await Photo.findAll({
+    where: { imovel_id: imovelId },
+    attributes: ["photo_id", "imageData"],
+  });
+}
+
+async function deletarImagem(photoId) {
+  const imagem = await Photo.findByPk(photoId);
+
+  if (!imagem) {
+    throw new Error("Imagem não encontrada");
+  }
+
+  await imagem.destroy();
+  return { message: "Imagem deletada com sucesso" };
+}
+
 async function deletarImovel(id) {
-  const imovel = await Imovel.findByPk(id);
+  const imovel = await Imovel.findByPk(id, {
+    include: [{ model: Photo, as: "photos" }],
+  });
 
   if (!imovel) {
-    throw new Error('Imóvel não encontrado');
+    throw new Error("Imóvel não encontrado");
+  }
+
+  if (imovel.photos.length > 0) {
+    await Photo.destroy({ where: { imovel_id: id } });
   }
 
   await imovel.destroy();
-  return { message: 'Imóvel deletado com sucesso' };
+  return { message: "Imóvel deletado com sucesso" };
 }
 
 module.exports = {
@@ -93,5 +88,7 @@ module.exports = {
   criarImovel,
   buscarImovelPorId,
   atualizarImovel,
-  deletarImovel
+  deletarImovel,
+  buscarImagensPorImovelId,
+  deletarImagem,
 };
